ca1c025a1120d74ee2efc6d3354ad79b238b8ab9
daishuaiqing <daishuaiqing@163.com>
2019-09-09 10:22:37 +0800

    更新

---
+++ src/views/Index.vue
@@ -0,0 +1,104 @@
+<template>
+  <!-- 进入界面 -->
+  <div>
+    <!-- <div class="header"></div> -->
+    <!-- 内容 -->
+    <div class="contain">
+      <div class="title">
+        <div class="title-item">
+          <div>
+            <img src="../assets/image/img-b155b7.png" alt />
+          </div>
+          <div style="font-size:28px">智能枪支弹药专用保险柜</div>
+          <div style="font-size:38px">管理系统</div>
+        </div>
+      </div>
+      <div class="cur-date">
+        <div class="title-item">
+          <div style="font-size:38px">2019年8月19日</div>
+          <div style="font-size:38px">15:06:44</div>
+        </div>
+      </div>
+    </div>
+    <!-- 底部仪表盘 -->
+    <div class="foot">
+      <div class="dash-board">网络正常</div>
+      <div class="dash-board">数据正常</div>
+      <div class="dash-board">电源正常</div>
+      <div class="dash-board">柜机正常</div>
+      <div class="dash-board">30℃</div>
+      <div class="dash-board">62%</div>
+    </div>
+    <!-- 底部 -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+.foot {
+  width: 100%;
+  height: 40px;
+  background: #7059ab;
+  position: absolute;
+  bottom: 0;
+  padding: 2% 20%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 1.1;
+  align-items: center;
+  color: #fff;
+}
+.contain {
+  position: absolute;
+  top: 0px;
+  bottom: 40px;
+  left: 0;
+  right: 0;
+  background: #5f4c9d;
+  color: #fff;
+  display: flex;
+  flex-direction: row;
+}
+.title {
+  width: 30%;
+  text-align: center;
+  margin-left: 18%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+.title img {
+  width: 45%;
+}
+.cur-date {
+  width: 30%;
+  text-align: center;
+  margin-left: 4%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+.title-item {
+  width: 100%;
+  height: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
+  background: #7059ab;
+  border-radius: 10px;
+}
+</style>
---
+++ src/views/login/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="contain">
+    <div class="header">
+      <div class="back">返回</div>
+      <div class="title">欢迎,请进行身份验证</div>
+      <div class="pass-login">密码登录</div>
+    </div>
+    <div class="main"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.header {
+  width: 100%;
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  text-align: center;
+  background: #7456a2;
+  font-size: 20px;
+  color: white;
+  height: 8%;
+  align-items: center;
+}
+.back {
+  width: 15%;
+  border-right: 1px solid white;
+}
+.title {
+  width: 70%;
+}
+.pass-login {
+  width: 15%;
+  border-left: 1px solid white;
+}
+.main {
+  position: absolute;
+  background: #a27bd4;
+  height: 92%;
+  top: 8%;
+  width: 100%;
+}
+</style>
